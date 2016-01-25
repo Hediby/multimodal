4d0e6940042755235c64fd8b00009e90e8eb201f
HediBY <hedi.benyounes@gmail.com>
2016-01-25 10:45:46 +0100

    Modifications

--- sim_model.py
+++ sim_model.py
@@ -10,39 +10,47 @@ import theano
 import theano.tensor as T
 import cPickle
 fwrite = sys.stdout.write
-dtype = 'float32'
+dtype = theano.config.floatX
 
 epsilon = 1e-6
 class Multimodal(object):
     @classmethod
-    def create(cls, n_img, n_txt, n_hid, l2=None):
+    def create(cls, n_img, n_txt, n_hid, l2=None, pooling=None, optim='sgd'):
         W_img_val = 0.01*np.random.randn(n_img, n_hid).astype(dtype)
         W_txt_val = 0.01*np.random.randn(n_txt, n_hid).astype(dtype)
-        return cls(W_img_val, W_txt_val, l2)
+        return cls(W_img_val, W_txt_val, l2, pooling, optim)
     
     @classmethod
     def load(cls,path):
-        W_img_val, W_txt_val, l2 = cPickle.load(open(path,'r'))
-        return cls(W_img_val, W_txt_val, l2)
+#        W_img_val, W_txt_val, l2, pooling, optim = cPickle.load(open(path,'r'))
+        params = cPickle.load(open(path,'r'))
+        W_img_val, W_txt_val, l2 = params[:3]
+        if len(params)<4:
+            pooling = None
+        if len(params)<5:
+            optim = 'sgd'
+        return cls(W_img_val, W_txt_val, l2, pooling, optim)
         
     def save(self, path):
-        to_save = [self.W_img.get_value(), self.W_txt.get_value(), self.l2]
+        to_save = [self.W_img.get_value(), self.W_txt.get_value(), self.l2, self.pooling, self.optim]
         cPickle.dump(to_save, open(path,'w'))
         return None
         
-    def __init__(self,W_img_val, W_txt_val, l2):
+    def __init__(self,W_img_val, W_txt_val, l2, pooling, optim):
+        self.optim = optim
+        self.pooling = pooling
         self.l2 = l2
         self.W_img = theano.shared(W_img_val, name='W_img')
         self.W_txt = theano.shared(W_txt_val, name='W_txt')
         self.params = [self.W_img, self.W_txt]
         
-        self.image = T.fvector('image')
+        self.image = T.vector('image')
         self.emb_image = T.dot(self.image, self.W_img)
         
-        self.text_p = T.fmatrix('text_p')
+        self.text_p = T.matrix('text_p')
         self.emb_text_p = T.dot(self.text_p, self.W_txt)
         
-        self.text_n = T.ftensor3('text_n')
+        self.text_n = T.tensor3('text_n')
         self.emb_text_n = T.dot(self.text_n, self.W_txt)
         
         
@@ -53,14 +61,32 @@ class Multimodal(object):
         
         def sim_3(x,y):
             x_r = x /  x.norm(2)
-            norms = y.norm(2, axis=2)
+            norms = y.norm(2, axis=-1)
             norms = T.reshape(norms, (norms.shape[0], norms.shape[1], 1))
             y_r = y / (epsilon + norms)
             return T.dot(y_r,x_r)
+        def sim_3_mean(x,y):
+            x_r = x /  x.norm(2)
+            norms = y.norm(2, axis=-1)
+            norms = T.reshape(norms, (norms.shape[0], 1))
+            y_r = y / (epsilon + norms)
+            return T.dot(y_r,x_r)
             
-        self.sim_p = T.max(sim_2(self.emb_image, self.emb_text_p.T))
-        self.sim3 = sim_3(self.emb_image, self.emb_text_n)
-        self.sim_n = T.max(self.sim3, axis=1)
+        if pooling == 'mean':
+            self.sim_p = sim_2(self.emb_image, T.mean(self.emb_text_p, axis=1))
+            self.sim_n = sim_3_mean(self.emb_image, T.mean(self.emb_text_n, axis=1))
+        if pooling=='softmax':
+            self.cos_p = sim_2(self.emb_image, self.emb_text_p.T)
+            self.cos_n = sim_3(self.emb_image, self.emb_text_n)
+            self.pre_sim_p = T.nnet.softmax(self.cos_p)
+            self.pre_sim_n = T.nnet.softmax(self.cos_n)
+            self.sim_p = T.max(self.pre_sim_p, axis=1)
+            self.sim_n = T.max(self.pre_sim_n, axis=1)
+        else:
+            self.cos_p = sim_2(self.emb_image, self.emb_text_p.T)
+            self.cos_n = sim_3(self.emb_image, self.emb_text_n)
+            self.sim_p = T.max(self.cos_p)
+            self.sim_n = T.max(self.cos_n, axis=1)
         self.maximum = T.maximum(0, 0.5 + self.sim_n - self.sim_p)
         self.cost = T.sum(self.maximum)
         
@@ -68,10 +94,18 @@ class Multimodal(object):
             for p in self.params:
                 self.cost += (T.sum(p**2))
 #        
-        self.lr = T.fscalar('lr')
+        self.lr = T.scalar('lr')
         self.grads = T.grad(self.cost, self.params)
-        self.updates = [(p, p-self.lr*g) for p,g in zip(self.params, self.grads)]
-
+        self.updates = []
+        if self.optim == 'sgd':
+            self.updates = [(p, p-self.lr*g) for p,g in zip(self.params, self.grads)]
+            
+        elif self.optim == 'rmsprop':
+            self.rms = [theano.shared(p.get_value()*0., name='rms_%s'%p) for p in self.params]
+            for p, r, g in zip(self.params, self.rms, self.grads):
+                new_r = 0.9*r + 0.1* g**2
+                self.updates.append((r, new_r))
+                self.updates.append((p, p-g*self.lr/(new_r+epsilon)))
         self._train = theano.function(inputs=[self.image, 
                                               self.text_p, 
                                               self.text_n, 
---
+++ train_sim_max.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 11:54:09 2016
+
+@author: hedi
+
+Uses file yielded by create_similarity_data.py for training a model that 
+computes a projection of images and texts based upon our model.
+"""
+import numpy as np
+import sys
+from time import time
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sim_model import Multimodal
+import theano
+fwrite = sys.stdout.write
+dtype = theano.config.floatX
+
+        
+if __name__=='__main__':
+    np.random.seed(1234)
+    which_set = 'r'
+    fwrite('Loading data...')
+    data = np.load(which_set+'_similarity_data_save').all()
+    fwrite(' Ok\n')
+    pooling = 'max' #mean, max or softmax
+    optim = 'sgd' #sgd or rmsprop (but rmsprop is really slow...)
+    folder = os.path.join(which_set+'_sim', pooling)
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    saving_path = os.path.join(folder, "model.mod")
+    
+    d = data[data.keys()[0]]
+    dim_img = d['image_emb'].shape[0]
+    dim_txt = d['text_emb'].shape[1]
+    dim_multi = 150
+    
+    image_embeddings = []
+    text_embeddings = []
+    product_ids = []
+    fwrite('Loading data ...\n')
+    product_ids = data.keys()
+    for idx in product_ids:
+        d = data.pop(idx)
+        if not len(data)%5000:
+            fwrite('%d\n' % len(data))
+        text_embeddings.append(d['text_emb'].astype(dtype))
+        image_embeddings.append(d['image_emb'].astype(dtype))
+    del data
+    n_data = len(product_ids)
+    fwrite('Done\n')
+    product_ids = np.array(product_ids[:n_data])
+    image_embeddings = np.array(image_embeddings[:n_data])
+    text_embeddings = np.array([t for t in text_embeddings[:n_data]])
+    
+    
+    indexes = np.arange(n_data)
+    np.random.shuffle(indexes)
+    
+    image_embeddings = image_embeddings[indexes]
+    text_embeddings = text_embeddings[indexes]
+    product_ids = product_ids[indexes]
+    
+    
+    n_train = int(0.7*n_data)
+    Xim_train = image_embeddings[:n_train]
+    Xim_test = image_embeddings[n_train+1:]
+    Xtxt_train = text_embeddings[:n_train]
+    Xtxt_test = text_embeddings[n_train+1:]
+    product_ids_train = product_ids[:n_train]
+    product_ids_test = product_ids[n_train + 1 :]
+    
+    test_lengths = [x.shape[0] for x in Xtxt_test]
+    test_ends = np.cumsum(test_lengths)
+    Xtxt_test_c = np.concatenate(Xtxt_test)
+    n_test = Xim_test.shape[0]
+
+
+    lr = 0.01
+    l2 = 0.
+    K = 5
+    
+    model = Multimodal.create(dim_img, dim_txt, dim_multi, l2, pooling, optim)
+#    
+    fwrite('Start training\n')
+    sys.stdout.flush()
+    break_all = False
+    n_updates = 0
+    for epoch in np.arange(51):
+        train_idxs = np.arange(n_train)
+        np.random.shuffle(train_idxs)
+        product_ids_train = product_ids_train[train_idxs]
+        Xim_train = Xim_train[train_idxs]
+        Xtxt_train = Xtxt_train[train_idxs]
+        fwrite('Epoch: %d\n' % epoch)
+        
+#==============================================================================
+# Test        
+#==============================================================================
+        
+        if not epoch%5:
+            W_txt = model.W_txt.get_value()
+            W_img = model.W_img.get_value()
+            Xim_test_m = np.dot(Xim_test, W_img)
+            
+            Xtxt_test_m = np.dot(Xtxt_test_c, W_txt)
+            sims = cosine_similarity(Xim_test_m, Xtxt_test_m)
+            b = 0
+            S = []
+            for e in test_ends:
+                s = sims[:,b:e].max(axis=1)
+                b = e
+                S.append(s)
+            S = np.array(S)  
+            
+            args = np.argsort(-S, axis=1)
+            test_ranks = [(arg == idx).nonzero()[0][0] for idx, arg in enumerate(args)]
+              
+            d = S.diagonal()
+            a = args[:,0]
+            s = S[np.arange(n_test), a]
+            
+            
+            plt.figure(figsize=(15,15))
+            plt.title('Rank histogram epoch %d'% epoch)
+            sns.distplot(test_ranks, label='Rank of the correct label (median = %d)' % np.median(test_ranks))
+            plt.legend()
+            plt.savefig(os.path.join(folder, 'rank_%d' %epoch))
+            
+            
+            plt.figure(figsize=(15,15))
+            plt.xlim([-0.1, 1.1])
+            plt.title('Similarities epoch %d' % epoch)
+            sns.distplot(d, label='Correct (im,txt)')
+            sns.distplot(s, label='Best (im,txt)')
+            plt.legend()
+            plt.savefig(os.path.join(folder, 'sims_%d' %epoch))
+            
+            median_rank = np.median(test_ranks)
+            fwrite('\tTest median rank = %d\n' % median_rank)
+            sys.stdout.flush()
+#==============================================================================
+# Train
+#==============================================================================
+        tic = time()
+        for image, text_p in zip(Xim_train, Xtxt_train):
+            d_neg = np.random.choice(n_train, size=K, replace=False)
+            xtxt_n = text_embeddings[d_neg]
+            n_updates += 1
+            cost = model.train(image, text_p, xtxt_n, lr)
+            if np.isnan(cost):
+                fwrite('\n NAN\n')
+                break_all = True
+            if break_all:
+                break
+        toc = time() - tic
+        model.save(saving_path)
+        fwrite('\tTime = %fs\n' % toc)
+        if break_all:
+            break
